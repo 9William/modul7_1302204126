45393c11e0f30a82662ba868972e0b3997765f48
Gerry William <gwilliam@student.telkomuniversity.ac.id>
2022-04-09 15:41:41 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace modul7_1302204126
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
